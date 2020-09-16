b52acffc5e576cbc0792407ab8a29f229070725e
Amit Davidson <amdavidson@paloaltonetworks.com>
2020-09-16 23:11:55 +0300

    Move analysis to function

--- pointerAnalysis.go
+++ pointerAnalysis.go
@@ -1,3 +1,50 @@
 package main
 
+import (
+	"StaticRaceDetector/domain"
+	"fmt"
+	"go/token"
+	"golang.org/x/tools/go/pointer"
+	"golang.org/x/tools/go/ssa"
+)
 
+func Analysis(pkg *ssa.Package, prog *ssa.Program, accesses []*domain.GuardedAccess) {
+	config := &pointer.Config{
+		Mains: []*ssa.Package{pkg},
+	}
+
+	valuesQueriesToGuardAccess := map[token.Pos]*domain.GuardedAccess{}
+	for _, guardedAccess := range accesses {
+		if pointer.CanPoint(guardedAccess.Value.Type()) {
+			config.AddQuery(guardedAccess.Value)
+			valuesQueriesToGuardAccess[guardedAccess.Value.Pos()] = guardedAccess
+		}
+	}
+
+	positionsToGuardAccesses := map[token.Pos][]*domain.GuardedAccess{}
+	result, err := pointer.Analyze(config)
+	if err != nil {
+		panic(err) // internal error in pointer analysis
+	}
+
+	for v, l := range result.Queries {
+		for _, label := range l.PointsTo().Labels() {
+			guardedAccess := valuesQueriesToGuardAccess[v.Pos()]
+			allocPos := label.Value()
+			positionsToGuardAccesses[allocPos.Pos()] = append(positionsToGuardAccesses[allocPos.Pos()], guardedAccess)
+		}
+	}
+	for _, guardedAccesses := range positionsToGuardAccesses {
+		for _, guardedAccessesA := range guardedAccesses {
+			for _, guardedAccessesB := range guardedAccesses {
+				if !guardedAccessesA.Intersects(guardedAccessesB) {
+					valueA := guardedAccessesA.Value
+					valueB := guardedAccessesB.Value
+					label := fmt.Sprintf(" %s with pos:%s has race condition with %s pos:%s \n", valueA, prog.Fset.Position(valueA.Pos()), valueB, prog.Fset.Position(valueB.Pos()))
+					print(label)
+				}
+			}
+		}
+	}
+
+}
--- summary_test.go
+++ summary_test.go
@@ -5,11 +5,7 @@ import (
 	"StaticRaceDetector/testutils"
 	"StaticRaceDetector/utils"
 	"encoding/json"
-	"fmt"
 	"github.com/stretchr/testify/require"
-	"go/token"
-	"golang.org/x/tools/go/pointer"
-	"golang.org/x/tools/go/ssa"
 	"testing"
 )
 
@@ -24,9 +20,9 @@ func TestGetFunctionSummary(t *testing.T) {
 		//{name: "Lock", testPath: "testutils/Lock/prog1.go", resPath: "testutils/Lock/prog1_expected"},
 		//{name: "LockAndUnlock", testPath: "testutils/LockAndUnlock/prog1.go", resPath: "testutils/LockAndUnlock/prog1_expected"},
 		//{name: "LockAndUnlockIfBranch", testPath: "testutils/LockAndUnlockIfBranch/prog1.go", resPath: "testutils/LockAndUnlockIfBranch/prog1_expected"},
-		{name: "LockAndUnlockIfMap", testPath: "testutils/LockAndUnlockIfMap/prog1.go", resPath: "testutils/LockAndUnlockIfMap/prog1_expected"},
+		//{name: "LockAndUnlockIfMap", testPath: "testutils/LockAndUnlockIfMap/prog1.go", resPath: "testutils/LockAndUnlockIfMap/prog1_expected"},
 		//{name: "NestedFunctions", testPath: "testutils/NestedFunctions/prog1.go", resPath: "testutils/NestedFunctions/prog1_expected"},
-		//{name: "NestedFunctionsTest", testPath: "testutils/NestedFunctionsTest/prog1.go", resPath: "testutils/NestedFunctionsTest/prog1_expected"},
+		{name: "DataRaceMap", testPath: "testutils/DataRaceMap/prog1.go", resPath: "testutils/DataRaceMap/prog1_expected"},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -44,44 +40,7 @@ func TestGetFunctionSummary(t *testing.T) {
 				utils.UpdateFile(t, tc.resPath, dump)
 			}
 			testutils.CompareResult(t, tc.resPath, lsRet, guardedAccessRet)
-
-			config := &pointer.Config{
-				Mains: []*ssa.Package{ssaPkg},
-			}
-
-			valuesQueriesToGuardAccess := map[token.Pos]*domain.GuardedAccess{}
-			for _, guardedAccess := range guardedAccessRet {
-				if pointer.CanPoint(guardedAccess.Value.Type()) {
-					config.AddQuery(guardedAccess.Value)
-					valuesQueriesToGuardAccess[guardedAccess.Value.Pos()] = guardedAccess
-				}
-			}
-
-			positionsToGuardAccesses := map[token.Pos][]*domain.GuardedAccess{}
-			result, err := pointer.Analyze(config)
-			if err != nil {
-				panic(err) // internal error in pointer analysis
-			}
-
-			for v, l := range result.Queries {
-				for _, label := range l.PointsTo().Labels() {
-					guardedAccess := valuesQueriesToGuardAccess[v.Pos()]
-					allocPos := label.Value()
-					positionsToGuardAccesses[allocPos.Pos()] = append(positionsToGuardAccesses[allocPos.Pos()], guardedAccess)
-				}
-			}
-			for _, guardedAccesses := range positionsToGuardAccesses {
-				for _, guardedAccessesA := range guardedAccesses {
-					for _, guardedAccessesB := range guardedAccesses {
-						if !guardedAccessesA.Intersects(guardedAccessesB) {
-							valueA := guardedAccessesA.Value
-							valueB := guardedAccessesB.Value
-							label := fmt.Sprintf(" %s with pos:%s has race condition with %s pos:%s \n", valueA, ssaProg.Fset.Position(valueA.Pos()), valueB, ssaProg.Fset.Position(valueB.Pos()))
-							print(label)
-						}
-					}
-				}
-			}
+			Analysis(ssaPkg, ssaProg, guardedAccessRet)
 		})
 	}
 }
