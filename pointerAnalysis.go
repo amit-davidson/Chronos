8bf725532e574ef157050519534eb13c1d021cfd
Amit Davidson <amdavidson@paloaltonetworks.com>
2020-09-21 23:21:57 +0300

    Fix and simplify pointer analysis

--- pointerAnalysis.go
+++ pointerAnalysis.go
@@ -9,17 +9,22 @@ import (
 	"golang.org/x/tools/go/ssa"
 )
 
+
+// Analysis starts by mapping between positions of the guard accesses (values inside) to the guard accesses themselves.
+// Then it analyzes all the values inside values inside, and check if some of the values might alias each other. If so,
+// the positions for those values are merged. After all positions were merged, the algorithm runs and check if for a
+// given value (identified by a pos in the map) there are two guarded accesses that might conflict - W&W/R&W from two
+// different goroutines.
 func Analysis(pkg *ssa.Package, prog *ssa.Program, accesses []*domain.GuardedAccess) {
 	config := &pointer.Config{
 		Mains: []*ssa.Package{pkg},
 	}
 
 	positionsToGuardAccesses := map[token.Pos][]*domain.GuardedAccess{}
-	valuesQueriesToGuardAccess := map[token.Pos]*domain.GuardedAccess{}
 	for _, guardedAccess := range accesses {
 		if pointer.CanPoint(guardedAccess.Value.Type()) {
 			config.AddQuery(guardedAccess.Value)
-			valuesQueriesToGuardAccess[guardedAccess.Value.Pos()] = guardedAccess
+			// Multiple instructions for the same variable for example write and multiple reads
 			positionsToGuardAccesses[guardedAccess.Value.Pos()] = append(positionsToGuardAccesses[guardedAccess.Value.Pos()], guardedAccess)
 		}
 	}
@@ -31,9 +36,12 @@ func Analysis(pkg *ssa.Package, prog *ssa.Program, accesses []*domain.GuardedAcc
 
 	for v, l := range result.Queries {
 		for _, label := range l.PointsTo().Labels() {
-			guardedAccess := valuesQueriesToGuardAccess[v.Pos()]
-			allocPos := label.Value()
-			positionsToGuardAccesses[allocPos.Pos()] = append(positionsToGuardAccesses[allocPos.Pos()], guardedAccess)
+			allocPos := label.Value().Pos()
+			queryPos := v.Pos()
+			if allocPos == queryPos {
+				continue
+			}
+			positionsToGuardAccesses[allocPos] = append(positionsToGuardAccesses[allocPos], positionsToGuardAccesses[queryPos]...)
 		}
 	}
 
