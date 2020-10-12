3d3db8f2f30ee2fdd7c8d6f80eb1aad629b9234d
Amit Davidson <amdavidson@paloaltonetworks.com>
2020-10-12 23:17:25 +0300

    Add main

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"StaticRaceDetector/domain"
+	"StaticRaceDetector/pointerAnalysis"
+	"StaticRaceDetector/ssaUtils"
+	"flag"
+	"fmt"
+	"golang.org/x/tools/go/ssa"
+	"os"
+)
+
+func main() {
+	file := flag.String("file", "", "The file containing the entry point of the function")
+	flag.Parse()
+	if *file == "" {
+		fmt.Printf("Please provide a file to load\n")
+		os.Exit(1)
+	}
+	ssaProg, ssaPkg, err := ssaUtils.LoadPackage(*file)
+	if err != nil {
+		fmt.Printf("Failed loading with the following error:%s\n", err)
+		os.Exit(1)
+	}
+	ssaUtils.SetGlobalProgram(ssaProg)
+
+	entryFunc := ssaPkg.Func("main")
+	entryCallCommon := ssa.CallCommon{Value: entryFunc}
+	functionState := ssaUtils.HandleCallCommon(domain.NewEmptyContext(), &entryCallCommon, entryFunc.Pos())
+	pointerAnalysis.Analysis(ssaPkg, ssaProg, functionState.GuardedAccesses)
+
+}
